d0b1cf7c17ea78735aa335d28a0d4e621b6c4eea
msaleembhatti <msaleembhatti@hotmail.com>
2020-01-19 19:31:06 +0500

    A new println is added to main.rs

--- q2/src/main.rs
+++ q2/src/main.rs
@@ -2,5 +2,7 @@ use Karim::BookStore::Book;
 use Karim::BookStore::english;
 fn main() {
     let java = Book::new("java".to_string(),"Dietel&Dietel".to_string() );    
+    
     english::print_book(java);
+    println!("changed FIle");
 }
